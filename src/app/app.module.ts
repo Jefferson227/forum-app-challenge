0ebebb8c01114d2822a47e6025188372ddc816f8
Jefferson Matheus <jefferson227@gmail.com>
2018-01-19 01:53:47 -0300

    feat(all): add navbar-top component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarTopComponent } from './navbar-top/navbar-top.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarTopComponent
   ],
   imports: [
     BrowserModule
