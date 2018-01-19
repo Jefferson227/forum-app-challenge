073f2f57569dad7e4343720408c7bddfaa67d3d4
Jefferson Matheus <jefferson227@gmail.com>
2018-01-19 18:11:46 -0300

    feat(all): add categories component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarTopComponent } from './navbar-top/navbar-top.component';
 import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarTopComponent,
-    BreadcrumbComponent
+    BreadcrumbComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule
