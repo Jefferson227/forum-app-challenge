86bc1059368ce27798887cbbd8ae86db4cb583eb
Jefferson Matheus <jefferson227@gmail.com>
2018-01-20 00:15:31 -0300

    feat(all): add post-content component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,10 @@ import { HomeComponent } from './components/home/home.component';
 import { CategoryComponent } from './components/category/category.component';
 import { PostsComponent } from './components/posts/posts.component';
 import { PostComponent } from './components/post/post.component';
+import { PostContentComponent } from './components/post-content/post-content.component';
 
 const appRoutes: Routes = [
+  { path: 'post-content', component: PostContentComponent },
   { path: 'posts', component: PostsComponent },
   { path: '**', component: HomeComponent }
 ];
@@ -24,7 +26,8 @@ const appRoutes: Routes = [
     HomeComponent,
     CategoryComponent,
     PostsComponent,
-    PostComponent
+    PostComponent,
+    PostContentComponent
   ],
   imports: [
     BrowserModule,
