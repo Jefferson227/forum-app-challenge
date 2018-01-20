fd63078817d3a6d075bedb03be56e24f97a9e8f8
Jefferson Matheus <jefferson227@gmail.com>
2018-01-19 23:19:46 -0300

    config(all): config route module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { NavbarTopComponent } from './components/navbar-top/navbar-top.component';
 import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
 import { CategoriesComponent } from './components/categories/categories.component';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { HomeComponent } from './components/home/home.component';
 
+const appRoutes: Routes = [
+  { path: 'posts', component: PostListComponent },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarTopComponent,
     BreadcrumbComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    PostListComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
