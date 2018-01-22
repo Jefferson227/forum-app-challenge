832a364c58cf7db7e2a618217970d23cdb537bfc
Jefferson Matheus <jefferson227@gmail.com>
2018-01-22 13:21:10 -0300

    fix(navbar-top): remove workaround
    Remove workaround to get user data

--- src/app/components/navbar-top/navbar-top.component.ts
+++ src/app/components/navbar-top/navbar-top.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-top',
@@ -9,21 +9,14 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class NavbarTopComponent implements OnInit {
   username;
 
-  constructor(
-    private _router: Router,
-    private _route: ActivatedRoute
-  ) {
-    // TODO: implement a better solution than this, with no workarounds
-    this._route.url.subscribe((url) => {
-      let user = localStorage.getItem('user')
-                   ? JSON.parse(localStorage.getItem('user'))
-                   : localStorage.getItem('user');
-
-      this.username = user.displayName;
-    });
-  }
+  constructor(private _router: Router) { }
 
   ngOnInit() {
+    let user = localStorage.getItem('user')
+                 ? JSON.parse(localStorage.getItem('user'))
+                 : localStorage.getItem('user');
+
+    this.username = user.displayName;
   }
 
   logout() {
