186ef9322507a46df2bb8e61fe6678285cc26396
Jefferson Matheus <jefferson227@gmail.com>
2018-01-21 23:48:45 -0300

    chore(navbar-top): get data from localStorage
    Actually this is an workaround because there is a proper solution for
    this problem, I just couldn't find it yet

--- src/app/components/navbar-top/navbar-top.component.ts
+++ src/app/components/navbar-top/navbar-top.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-top',
@@ -7,8 +7,20 @@ import { Router } from '@angular/router';
   styleUrls: ['./navbar-top.component.scss']
 })
 export class NavbarTopComponent implements OnInit {
+  username;
 
-  constructor(private _router: Router) {
+  constructor(
+    private _router: Router,
+    private _route: ActivatedRoute
+  ) {
+    // TODO: implement a better solution than this, with no workarounds
+    this._route.url.subscribe((url) => {
+      let user = localStorage.getItem('user')
+                   ? JSON.parse(localStorage.getItem('user'))
+                   : localStorage.getItem('user');
+
+      this.username = user.displayName;
+    });
   }
 
   ngOnInit() {
