a173c3f5685348c94f00c68f5b0e24f9ca7686d5
Jefferson Matheus <jefferson227@gmail.com>
2018-01-23 02:22:14 -0300

    chore(discussions): convert timestamp into date

--- src/app/components/discussions/discussions.component.ts
+++ src/app/components/discussions/discussions.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseService } from '../../providers/firebase/firebase.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-discussions',
@@ -13,14 +14,23 @@ export class DiscussionsComponent implements OnInit {
     const discussionObservable = this._firebaseService.getAllDiscussions();
 
     discussionObservable.on('value', snap => {
-      // console.log(snap.val());
       const firebaseObj = snap.val();
-      this.discussions = Object.keys(firebaseObj).map(item => firebaseObj[item]);
+
+      this.discussions = Object.keys(firebaseObj).map(item => {
+        let _item = firebaseObj[item];
+        _item.timestamp = this.transformDate(_item.timestamp);
+
+        return _item;
+      });
       console.log(this.discussions);
     });
   }
 
   ngOnInit() {
+  }
+
+  transformDate(timestamp) {
+    return moment(timestamp).format('YYYY-MM-DD hh:mm:ss');
   }
 
 }
