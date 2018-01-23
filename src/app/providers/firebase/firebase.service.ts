31eb23a252b9d6876556653ae6ea4976728a5eca
Jefferson Matheus <jefferson227@gmail.com>
2018-01-22 23:43:41 -0300

    chore(firebase): implement login and logout
    Login and logout for accounts by email

--- src/app/providers/firebase/firebase.service.ts
+++ src/app/providers/firebase/firebase.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class FirebaseService {
 
   firebaseInstance;
+  firebaseAuth;
 
-  constructor() { 
+  constructor(private _router: Router) {
     this.initializeFirebase();
   }
 
   initializeFirebase() {
+    // Configuration parameters
     let config = {
       apiKey: "AIzaSyA3A60PNVTjLDhXNpVKDhjntcp7KSsWAp4",
       authDomain: "forum-app-challenge.firebaseapp.com",
@@ -19,12 +22,38 @@ export class FirebaseService {
       messagingSenderId: "937135036055"
     };
 
+    // Initializing connection with firebase
     this.firebaseInstance = firebase;
     this.firebaseInstance.initializeApp(config);
+    this.firebaseAuth = this.firebaseInstance.auth();
+
+    // Activating listener for user logged by email and password
+    this.onAuthStateChanged();
   }
 
   authenticateWithGoogle() {
     let provider = new firebase.auth.GoogleAuthProvider();
-    return this.firebaseInstance.auth().signInWithPopup(provider);
+    return this.firebaseAuth.signInWithPopup(provider);
+  }
+
+  authenticateWithEmailAndPassword(email, password) {
+    this.firebaseAuth
+      .signInWithEmailAndPassword(email, password)
+      .catch(err => console.error(err));
+  }
+
+  onAuthStateChanged() {
+    this.firebaseAuth
+      .onAuthStateChanged(user => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this._router.navigate(['/']);
+        }
+      });
+  }
+
+  signOut() {
+    localStorage.clear();
+    this.firebaseAuth.signOut();
   }
 }
