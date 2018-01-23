c28f6f086fe1cb2522d4338369ab82de0466dd23
Jefferson Matheus <jefferson227@gmail.com>
2018-01-23 19:50:53 -0300

    refactor(discussions): refactor transformTimestamp

--- src/app/components/discussions/discussions.component.ts
+++ src/app/components/discussions/discussions.component.ts
@@ -36,7 +36,41 @@ export class DiscussionsComponent implements OnInit {
   }
 
   transformDate(timestamp) {
-    return moment(timestamp).format('YYYY-MM-DD hh:mm:ss');
+    let diff = moment(new Date()).diff(timestamp, 'seconds');
+    let value = '';
+
+    if (diff > 60) {
+      diff = moment(new Date()).diff(timestamp, 'minutes');
+
+      if (diff > 60) {
+        diff = moment(new Date()).diff(timestamp, 'hours');
+
+        if (diff > 24) {
+          diff = moment(new Date()).diff(timestamp, 'days');
+
+          if (diff > 31) {
+            diff = moment(new Date()).diff(timestamp, 'months');
+
+            if (diff > 12) {
+              diff = moment(new Date()).diff(timestamp, 'years');
+              value = diff + 'y';
+            } else {
+              value = diff + 'mo';
+            }
+          } else {
+            value = diff + 'd';
+          }
+        } else {
+          value = diff + 'h';
+        }
+      } else {
+        value = diff + 'm';
+      }
+    } else {
+      value = diff + 's';
+    }
+
+    return value;
   }
 
 }
